4c45989633a3981b3e1c6eac2425a578619d1f5f
Анна Шахбазян <fatamorgana9205@gmail.com>
2021-05-21 12:18:20 +0300

    conference version basic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
+import { v4 as uuidV4 } from "uuid";
 
 const app = express();
 const server = http.createServer(app);
@@ -21,19 +22,49 @@ app.get("/", (req, res) => {
     res.send("Server is running");
 });
 
+app.get("/join", (req, res) => {
+    res.send({ link: uuidV4() });
+});
+
+const peers = {};
+
 io.on("connection", (socket) => {
+    peers[socket.id] = socket;
+    // Asking all other clients to setup the peer connection receiver
+    for (let id in peers) {
+        if (id === socket.id) continue;
+        console.log("sending init receive to " + socket.id);
+        peers[id].emit("initReceive", socket.id);
+    }
+
+    /**
+     * relay a peerconnection signal to a specific socket
+     */
+    socket.on("signal", (data) => {
+        console.log("sending signal from " + socket.id + " to ", data);
+        if (!peers[data.socket_id]) return;
+        peers[data.socket_id].emit("signal", {
+            socket_id: socket.id,
+            signal: data.signal,
+        });
+    });
+
     socket.emit("me", socket.id);
+
     socket.on("disconnect", () => {
-        socket.broadcast.emit("callended");
+        console.log("socket disconnected " + socket.id);
+
+        socket.broadcast.emit("removePeer", socket.id);
+        delete peers[socket.id];
     });
 
-    socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-        io.to(userToCall).emit("calluser", { signal: signalData, from, name });
-    });
-
-    socket.on("answercall", (data) => {
-        console.log("answer", data);
-        io.to(data.to).emit("callaccepted", data.signal);
+    /**
+     * Send message to client to initiate a connection
+     * The sender has already setup a peer connection receiver
+     */
+    socket.on("initSend", (init_socket_id) => {
+        console.log("INIT SEND by " + socket.id + " for " + init_socket_id);
+        peers[init_socket_id].emit("initSend", socket.id);
     });
 });
 
